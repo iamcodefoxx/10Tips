90585e0086965c146a56bf2ea829c310548c8a93
iamcodefoxx <71807189+iamcodefoxx@users.noreply.github.com>
2020-10-31 11:20:37 -0700

    Delete script.js

--- #10Tips/script.js
+++
@@ -1,8 +0,0 @@
-function calculate(){
-  var billAmount = document.getElementById("bill-amount").value;
-  var shareBill = document.getElementById("share-bill").value;
-  var serviceQuality = document.getElementById("service-quality").value;
-
-  var result = ((billAmount * serviceQuality) / (shareBill)).toFixed(2);
-  document.getElementById("total").innerHTML = "$" + result;
-}
